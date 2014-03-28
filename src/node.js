83ff9b5943810c1e74653cdac5d0d2bf8a88a9aa
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2014-03-28 22:37:16 +0100

    Fix some issue and add comment

--- src/node.js
+++ src/node.js
@@ -3,21 +3,20 @@ var http = require("http");
 
 var homehue = express();
 
-homehue.get("/", function(req, res) {
-    res.sendfile("views/index.html");
-})
-.get("/dist/*", function(req, res) {
-    res.sendfile("." + req.url);
-})
-.get("/allLight", function(req, res) {
-    //console.log("GET --> /allLight");
+/*
+ * GET all light status
+ */
+homehue.get("/allLight", function(req, res) {
     createHueLightRequest("GET", null, "", function(response) {
         res.write(response);
         res.send();
     });
 })
+
+/*
+ * GET / SET one light state
+ */
 .get("/light/:id", function(req, res) {
-    //console.log("SET --> /light/" + req.params.id);
     if (req.query.data) {
         createHueLightRequest("PUT", req.params.id + "/state", req.query.data, function(response) {
             res.write(response);
@@ -30,12 +29,20 @@ homehue.get("/", function(req, res) {
         });
     }
 })
+
+/*
+ * STATIC and 404
+ */
+.use(express.static(__dirname + '/../views'))
+.use(express.static(__dirname + '/../'))
 .use(function(req, res, next){
-    console.log(req.url);
     res.setHeader("Content-Type", "text/plain");
     res.send(404, "Page Introuvable");
 });
 
+/*
+ * Create hue request with success callback
+ */
 function createHueLightRequest (method, path, body, success) {
     var options = {
       hostname: '192.168.1.12',
@@ -46,11 +53,9 @@ function createHueLightRequest (method, path, body, success) {
     var req = http.request(options, function(res) {
         var data = "";
         res.on('data', function (chunk) {
-            //console.log('RESPONSE DATA --> ' + chunk);
             data += chunk;
         })
         .on('end', function() {
-            //console.log('RESPONSE COMPLETE --> ' + data);
             if (success) {
                 success(data);
             }
@@ -65,4 +70,7 @@ function createHueLightRequest (method, path, body, success) {
     req.end();
 }
 
+/*
+ * Start to listen
+ */
 homehue.listen(process.env.PORT || 8080);
